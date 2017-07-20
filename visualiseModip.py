03aa0451a48bdb1d4f5ccd0dab47a7db6ed3540c
Ping Liang Tan <tpl2go@gmail.com>
2017-07-20 10:07:55 +0200

    removed unnecessary files

--- visualiseModip.py
+++
@@ -1,9 +0,0 @@
-from ComputeModip import *
-import matplotlib.pyplot as plt
-
-def visualise_modip_file():
-    data = read_stMoDIP('/home/tpl/Documents/Airbus/Project/Papers/Nequick/CCIR_MoDIP/modipNeQG_wrapped.txt')
-    plt.contour(data)
-    plt.show()
-
-visualise_modip_file()
