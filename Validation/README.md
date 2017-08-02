01e4bb7599c7b23ce9de1d962a1b9fc54fa8a371
Ping Liang Tan <tpl2go@gmail.com>
2017-08-02 15:37:47 +0200

    added validation reference files

--- Validation/README.md
+++ Validation/README.md
@@ -1,5 +1,8 @@
 # Validation
 ESA's Galileo Ionospheric Correction document comes with three validation data tables (High, Medium, and Low solar activities)
+
 They have been copied into three text files for convenience: High_reference.dat, Medium_reference.dat Low_reference.dat
+
 Each table describes inputs to the slant TEC calculation and the expected output.
-This validation scripts calls the NequickG routine and compares the computed values against the expected value
+
+This validation scripts calls the NequickG routine and compares the computed values against the expected value.
--- spheretrig.py
+++ spheretrig.py
@@ -38,7 +38,8 @@ def zenith(h1, lat1, lon1, h2, lat2, lon2):
         return zenith
 
 def azimuth(lat1, lon1, lat2, lon2):
-        """ azimuth of point 2 as seen from point 1"""
+        """ Imagine a bearing specified by a great arc from point 1 to point 2 on a
+        unit sphere. Azimuth angle is the angle from the north bearing to current bearing"""
 
         delta = greatcircle(lat1, lon1, lat2, lon2)
         DR = np.pi / 180
@@ -50,54 +51,3 @@ def azimuth(lat1, lon1, lat2, lon2):
         sine_sigma = (np.cos(lat2 * DR) * np.sin((lon2 - lon1) * DR)) / np.sin(delta * DR)
 
         return np.arctan2(sine_sigma, cosine_sigma) * 180/np.pi
-
-
-
-    # @staticmethod
-    # def compute_greatcircle(lat1, lon1, lat2, lon2):
-    #     """angle spanned by great arc connecting point 1 and 2
-    #     symbol in reference document: delta"""
-    #     DR = np.pi / 180.0
-    #
-    #     # Spherical law of cosines
-    #     cosine_delta = np.sin(lat1 * DR) * np.sin(lat2 * DR) + np.cos(lat1 * DR) * np.cos(lat2 * DR) * np.cos(
-    #         (lon2 - lon1) * DR)
-    #     sine_delta = np.sqrt(1 - cosine_delta ** 2)
-    #
-    #     return np.arctan2(sine_delta, cosine_delta) * 180 / np.pi
-    #
-    # def compute_zenith(self, h1, lat1, lon1, h2, lat2, lon2):
-    #     """
-    #     zenith angle of point 2 from point 1
-    #     :param lat1: [deg]
-    #     :param lon1: [deg]
-    #     :param lat2: [deg]
-    #     :param lon2: [deg]
-    #     :return: [deg]
-    #     """
-    #     delta = self.compute_greatcircle(lat1, lon1, lat2, lon2)
-    #
-    #     DR = np.pi / 180
-    #     cosine = np.cos(delta * DR)
-    #     sine = np.sin(delta * DR)
-    #
-    #     r1 = 6371.2 + h1
-    #     r2 = 6371.2 + h2
-    #
-    #     zenith = np.arctan2(sine, cosine - r1 / r2) * 180.0 / np.pi
-    #
-    #     return zenith
-    #
-    # def compute_azimuth(self, lat1, lon1, lat2, lon2):
-    #     """ azimuth of point 2 as seen from point 1"""
-    #
-    #     delta = self.compute_greatcircle(lat1, lon1, lat2, lon2)
-    #     DR = np.pi / 180
-    #
-    #     # spherical cosine rule on spherical triangle between pole and great circle
-    #     cosine_sigma = (np.sin(lat2 * DR) - np.sin(lat1 * DR) * np.cos(delta * DR)) / (
-    #         np.cos(lat1 * DR) * np.sin(delta * DR))
-    #     # spherical sine rule on spherical triangle between pole and great circle
-    #     sine_sigma = (np.cos(lat2 * DR) * np.sin((lon2 - lon1) * DR)) / np.sin(delta * DR)
-    #
-    #     return np.arctan2(sine_sigma, cosine_sigma) * 180/np.pi
