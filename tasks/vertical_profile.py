c9dde8b444b9bf38cd5b6a3449a6d6da71762c51
Ping Liang Tan <tpl2go@gmail.com>
2017-08-10 16:41:08 +0200

    parameterised plotting

--- tasks/vertical_profile.py
+++ tasks/vertical_profile.py
@@ -5,10 +5,18 @@ from NequickG import NEQTime, Position, GalileoBroadcast
 from NequickG_global import NequickG_global
 
 
+""" Input Parameters here """
+mth = 4
+UT = 12
+lat = 40
+lon = 100
+Az = 64
+
+""" Processing Below """
 # Create input objects
-TX = NEQTime(4,12)
-BX = GalileoBroadcast(236.831,0,0)
-RX = Position(40,0)
+TX = NEQTime(mth,UT)
+BX = GalileoBroadcast(Az,0,0)
+RX = Position(lat,lon)
 
 # Create Nequick models
 NEQ_global = NequickG_global(TX,BX)
@@ -22,11 +30,15 @@ N = NEQ.electrondensity(hs)
 Azr = Para.Azr
 
 # Plotting
-label = "Azr" + str(int(Azr)) + " Apr 12UT 40N 0E"
+label = ' lat:' + str(lat) + ' lon:' + str(lon)
 plt.plot(hs, N, label = label)
+plt.plot(Para.hmF2,Para.NmF2 * 10 ** 11, marker = 'o', markersize=5, linestyle='None', label = 'F2 anchor pt')
+plt.plot(Para.hmF1,Para.NmF1* 10 ** 11, marker = 'o', markersize=5, linestyle='None', label = 'F1 anchor pt')
+plt.plot(Para.hmE,Para.NmE* 10 ** 11, marker = 'o', markersize=5, linestyle='None', label = 'E anchor pt')
 plt.xlabel('Height(km)')
 plt.ylabel("Electron Density (m^-3)")
-plt.title("Nequick-G:\n" + " Apr 12UT ")
+plt.title("Nequick-G:\n" + 'mth:' + str(mth) + ' UT:' + str(UT) + ' Azr:' + str(int(Azr)))
 plt.grid()
 plt.legend()
 plt.show()
+
