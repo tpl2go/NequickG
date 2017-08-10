2d436756871c5abd19192ca94ef9dd236ecb014c
Ping Liang Tan <tpl2go@gmail.com>
2017-08-10 16:36:38 +0200

    visualise the trajectories specified in validation dataset

---
+++ visualise_arc.py
@@ -0,0 +1,48 @@
+from NequickG import GalileoBroadcast, NEQTime
+from NequickG_global import NequickG_global, Ray
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+
+def run(table_type):
+    """
+    Plot all trajectory of specified in validation table
+    """
+    print "Plotting trajectories in", table_type, "Solar Activity data table"
+    with open('./Validation/' + table_type + '_reference.dat') as infile:
+        reader = csv.reader(infile, delimiter = ' ')
+        plt.figure()
+        mapp = Basemap(projection='cyl',llcrnrlat= -90.,urcrnrlat= 90.,\
+                  resolution='c',  llcrnrlon=-180.,urcrnrlon=180.)
+        #-- draw coastlines, state and country boundaries, edge of map
+        mapp.drawcoastlines()
+        mapp.drawstates()
+        mapp.drawcountries()
+
+        #-- create and draw meridians and parallels grid lines
+        mapp.drawparallels(np.arange( -90., 90.,30.),labels=[1,0,0,0],fontsize=10)
+        mapp.drawmeridians(np.arange(-180.,180.,30.),labels=[0,0,0,1],fontsize=10)
+        for row in reader:
+            row = map(float, row)
+
+            lon1 = row[2]
+            lat1 = row[3]
+            h1 = row[4] / 1000 # change m to km
+
+            lon2 = row[5]
+            lat2 = row[6]
+            h2 = row[7] / 1000 # change m to km
+
+            ray = Ray(h1, lat1, lon1, h2, lat2, lon2)
+
+            rs, lats, lons, delta = ray.linspace(100)
+
+            x, y = mapp(lons, lats)
+            mapp.plot(x,y, marker='.', linestyle='None')
+
+        plt.savefig(table_type+'.png')
+
+run('Medium')
+run('Low')
+run('High')
