ea2cbea15ef2b7a894edba00f0e203b3d66e09b0
Ping Liang Tan <tpl2go@gmail.com>
2017-08-10 17:54:48 +0200

    corrected name error

--- CCIR_MoDIP/compile.py
+++ CCIR_MoDIP/compile.py
@@ -37,11 +37,11 @@ for mth in range(1, 13):
                 Fm3out.writelines('],\n')
             Fm3out.writelines(']\n\n')
 
-with open('F2.py', 'a') as F2out:
+with open('ccir_f2.py', 'a') as F2out:
     F2out.writelines(
         'F2 = {1: CCIR11_F2, 2: CCIR12_F2, 3: CCIR13_F2, 4: CCIR14_F2, 5: CCIR15_F2, 6: CCIR16_F2, 7: CCIR17_F2, 8: CCIR18_F2, 9: CCIR19_F2, 10: CCIR20_F2, 11: CCIR21_F2, 12: CCIR22_F2}')
 
-with open('Fm3.py', 'a') as Fm3out:
+with open('ccir_fm3.py', 'a') as Fm3out:
     Fm3out.writelines(
         'Fm3 = {1: CCIR11_FM3, 2: CCIR12_FM3, 3: CCIR13_FM3, 4: CCIR14_FM3, 5: CCIR15_FM3, 6: CCIR16_FM3, 7: CCIR17_FM3, 8: CCIR18_FM3, 9: CCIR19_FM3, 10: CCIR20_FM3, 11: CCIR21_FM3, 12: CCIR22_FM3}')
 
